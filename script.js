58ae0e203685b79e9046bb64680b4cc637177c67
defbit <115157208+defbit@users.noreply.github.com>
2023-11-03 12:11:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function contagem(){
+    let saida = document.getElementById('saida')
+    let num = Number(document.getElementById('fnum').value)
+
+    let cont = 0
+    while(cont <= num){
+        saida.innerHTML += " " + `${cont}`
+        cont++
+    }
+}
